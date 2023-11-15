a2657745b8eceb24a851deaaa5038a8087e6186c
chandanaannadi <103553008+chandanaannadi@users.noreply.github.com>
2023-11-15 17:29:23 -0600

    modified app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-var pizza = require('./models/pizza');
+//var pizza = require('./Models/pizza');
 
 // async function recreateDB(){
 //   // Delete everything
