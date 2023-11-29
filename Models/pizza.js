acefdad44272ff50d40ba1e5b691b026c8ef3cd9
chandanaannadi <103553008+chandanaannadi@users.noreply.github.com>
2023-11-29 00:21:31 -0600

    Modified code

--- Models/pizza.js
+++ Models/pizza.js
@@ -1,19 +1,19 @@
 const { Double } = require("mongodb")
 const mongoose = require("mongoose")
 const pizzaSchema = mongoose.Schema({
-pizza_name: {
-    type: String,
-    required: true
-},
-pizza_type: {
-    type: String,
-    required: true
-},
-pizza_price: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 500
-}
+    pizza_name: {
+        type: String,
+        required: true
+    },
+    pizza_type: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength:10
+    },
+    pizza_price: {
+        type: Number,
+        required: true,
+    }
 });
 module.exports = mongoose.model("pizza", pizzaSchema)
